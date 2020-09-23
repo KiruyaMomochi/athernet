d913065d47e1fe64c7f049756f1ed1cb86fd2f63
Haotian Jing <jinght@shanghaitech.edu.cn>
2020-09-23 23:21:23 +0800

    Modulator refactor

---
+++ athernet/Modulators/BinaryPSKModulator.cs
@@ -0,0 +1,55 @@
+﻿using athernet.Packets;
+using NAudio.Wave.SampleProviders;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace athernet.Modulators
+{
+    class BinaryPSKModulator : BinaryModulator
+    {
+        public BinaryPSKModulator(int sampleRate, double frequncy, double gain) :
+            base(sampleRate,
+                new double[] { frequncy, frequncy }, 
+                new double[] { gain, -gain })
+        { }
+
+        public override BitArray Demodulate(Packet packet)
+        {
+            int packetLength = packet.Length;
+            int bitLength = packetLength / SamplesPerBit;
+
+            SignalGenerator carrier = SignalGenertor();
+            Zero(carrier);
+
+            BitArray bitArray = new BitArray(bitLength);
+            int nSample = 0;
+
+            float[] carrierBuf = new float[SamplesPerBit];
+
+            for (int i = 0; i < bitLength; i++)
+            {
+                carrier.Read(carrierBuf, 0, SamplesPerBit);
+                float sum = 0;
+
+                for (int j = 0; j < SamplesPerBit; j++)
+                {
+                    sum += packet.Samples[nSample] * carrierBuf[j];
+                    nSample++;
+                }
+
+                if (sum > 0)
+                {
+                    bitArray.Set(i, true);
+                }
+                else
+                {
+                    bitArray.Set(i, false);
+                }
+            }
+
+            return bitArray;
+        }
+    }
+}
---
+++ athernet/Modulators/DifferentialBinaryModulator.cs
@@ -0,0 +1,52 @@
+﻿using athernet.Packets;
+using NAudio.Wave.SampleProviders;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace athernet.Modulators
+{
+    abstract class DifferentialBinaryModulator: BinaryModulator
+    {
+        private int lastIdx = 0;
+
+        protected override void One(SignalGenerator signal)
+        {
+            lastIdx ^= 1;
+            signal.Frequency = Frequency[lastIdx];
+            signal.Gain = Gain[lastIdx];
+        }
+
+        protected override void Zero(SignalGenerator signal)
+        {
+            // Do nothing
+        }
+
+        public DifferentialBinaryModulator(int sampleRate, double frequncy, double gain) :
+            base(sampleRate,
+                new double[] { frequncy, frequncy },
+                new double[] { gain, -gain })
+        { }
+
+        public override Packet Modulate(BitArray bitArray)
+        {
+            int packetLength = (bitArray.Length + 1) * SamplesPerBit;
+            Packet packet = new Packet(SampleRate, packetLength);
+            int nSample = 0;
+            SignalGenerator carrier = SignalGenertor();
+
+            nSample += carrier.Read(packet.Samples, nSample, SamplesPerBit);
+            foreach (bool bit in bitArray)
+            {
+                if (bit)
+                    One(carrier);
+                else
+                    Zero(carrier);
+                nSample += carrier.Read(packet.Samples, nSample, SamplesPerBit);
+            }
+
+            return packet;
+        }
+    }
+}
