cc8bbf32f2f3a6ae4767b5f8d30251a4c15c2d71
KuriyaMomochi <jinght@shanghaitech.edu.cn>
2020-09-25 00:54:28 +0800

    Add SineGenerator for phase changing

---
+++ athernet/SampleProviders/SineGenerator.cs
@@ -0,0 +1,48 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace athernet.SampleProviders
+{
+    class SineGenerator : ISampleProvider
+    {
+        private const double TwoPi = 2 * Math.PI;
+        private int nSample;
+
+        public SineGenerator() : this(48000, 1) { }
+
+        public SineGenerator(int sampleRate, int channel)
+        {
+            WaveFormat = WaveFormat.CreateIeeeFloatWaveFormat(sampleRate, channel);
+
+            // Default
+            Frequency = 440.0;
+            Gain = 1;
+            PhaseShift = 0.0;
+        }
+
+        public WaveFormat WaveFormat { get; }
+        public double Frequency { get; set; }
+        public double Gain { get; set; }
+        public double PhaseShift { get; set; }
+
+        public int Read(float[] buffer, int offset, int count)
+        {
+            // Generator current value
+            double multiple;
+            double sampleValue;
+            int outIndex = offset;
+
+            // Complete Buffer
+            for (int sampleCount = 0; sampleCount < count / WaveFormat.Channels; sampleCount++)
+            {
+                multiple = TwoPi * Frequency / WaveFormat.SampleRate;
+                sampleValue = Gain * Math.Sin(nSample * multiple + PhaseShift);
+                buffer[outIndex++] = (float)sampleValue;
+                nSample++;
+            }
+            return count;
+        }
+    }
+}
