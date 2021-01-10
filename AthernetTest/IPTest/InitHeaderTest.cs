2b34263401e393854780b2cf8613b5a6a41a894d
KiruyaMomochi <65301509+KiruyaMomochi@users.noreply.github.com>
2021-01-10 08:22:41 +0800

    Add test files

--- AthernetTest/IPTest/InitHeaderTest.cs
+++ AthernetTest/IPTest/InitHeaderTest.cs
@@ -15,7 +15,8 @@ namespace AthernetTest.IPTest
         private readonly ITestOutputHelper _testOutputHelper;
         private readonly IcmpHeader _icmpHeader;
         private readonly UdpHeader _udpHeader;
-
+        private readonly TcpHeader _tcpHeader;
+        
         public InitHeaderTest(ITestOutputHelper testOutputHelper)
         {
             _testOutputHelper = testOutputHelper;
@@ -32,6 +33,19 @@ namespace AthernetTest.IPTest
                 SourcePort = 42328,
                 DestinationPort = 2333,
                 Checksum = 0xa3b7
+            };
+            _tcpHeader = new TcpHeader()
+            {
+                SourcePort = 21,
+                DestinationPort = 64657,
+                SequenceNumber = 54398293,
+                AcknowledgementNumber = 3780304945,
+                DataOffset = 8,
+                Flags = TcpFlags.Acknowledgment | TcpFlags.Syn,
+                WindowSize = 65535,
+                Checksum = 0x6186,
+                UrgentPointer = 0,
+                Options = new byte[]{0x02, 0x04, 0x05, 0xb4, 0x01, 0x03, 0x03, 0x08, 0x01, 0x01, 0x04, 0x02}
             };
         }
 
@@ -107,6 +121,39 @@ namespace AthernetTest.IPTest
                 DestinationAddress = IPAddress.Parse("10.20.223.177")
             };
             var testPacket = ipv4Header.GetProtocolPacketBytes(_udpHeader, realPacket.Skip(28).ToArray());
+            _testOutputHelper.WriteLine(BitConverter.ToString(testPacket).Replace("-", ""));
+            Assert.True(testPacket.SequenceEqual(realPacket));
+        }
+
+        [Fact]
+        public void TestIpv4TcpHeader()
+        {
+            var real =
+                "45000034682f40007f0654d90a145b5c0a14cf370015fc91033e0d55e152e0318012ffff61860000020405b40103030801010402";
+            //   45000034682F40007F0654D90A145B5C0A14CF370015FC9161860D55E152E0318012FFFF00000000020405B40103030801010402
+            var realPacket = new List<byte>(real.Length / 2);
+            for (int i = 0; i < real.Length; i += 2)
+            {
+                realPacket.Add(Convert.ToByte(real.Substring(i, 2), 16));
+            }
+
+            var ipv4Header = new Ipv4Header()
+            {
+                Version = 4,
+                Length = 20,
+                TypeOfService = 0,
+                TotalLength = 52,
+                Id = 0x682f,
+                Flags = 0x40,
+                Offset = 0,
+                Ttl = 127,
+                Protocol = ProtocolType.Tcp,
+                Checksum = 0x54d9,
+                SourceAddress = IPAddress.Parse("10.20.91.92"),
+                DestinationAddress = IPAddress.Parse("10.20.207.55")
+            };
+
+            var testPacket = ipv4Header.GetProtocolPacketBytes(_tcpHeader, Array.Empty<byte>());
             _testOutputHelper.WriteLine(BitConverter.ToString(testPacket).Replace("-", ""));
             Assert.True(testPacket.SequenceEqual(realPacket));
         }
