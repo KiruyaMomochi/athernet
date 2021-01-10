b997ca7a51321d1259512a7d458fcb6ad28d9fee
KiruyaMomochi <65301509+KiruyaMomochi@users.noreply.github.com>
2021-01-10 16:00:58 +0800

    External icmp

--- Athernet/Nat/Nat.cs
+++ Athernet/Nat/Nat.cs
@@ -25,6 +25,7 @@ namespace Athernet.Nat
         private Random _random = new Random();
 
         private readonly PacketCommunicator _communicator;
+
         private IpV4Address _localAddress;
         // private IpV4Address _remoteAddress;
 
@@ -35,7 +36,7 @@ namespace Athernet.Nat
         {
             MacAddress remoteMacAddress;
             MacAddress localMacAddress;
-            
+
             _athernetMac = athernetMac;
             _natTable = new Dictionary<NatEntry, NatEntry>();
             var device = LivePacketDevice.AllLocalMachine[deviceIndex];
@@ -70,11 +71,12 @@ namespace Athernet.Nat
             {
                 if (_filterPorts.Count == 0)
                 {
-                    return "dst port 12312";
+                    return "icmp";
                 }
-                
+
                 // return "tcp and (" + string.Join(" or ", _filterPorts.Select(x => $"(dst port {x})")) + ")";
-                return string.Join(" or ", _filterPorts.Select(x => $"(dst port {x})"));
+                var tcp_str = string.Join(" or ", _filterPorts.Select(x => $"(dst port {x})"));
+                return "(icmp) or (" + tcp_str + ")";
             }
         }
 
@@ -86,47 +88,71 @@ namespace Athernet.Nat
         // SendEthernetPacket
 
         private readonly Dictionary<NatEntry, NatEntry> _natTable;
-        
+
         // receive only tcp & ports in table
         private void OnAthernetPacketAvailable(byte[] data)
         {
             var ipV4Datagram = new Packet(data, DateTime.Now, DataLinkKind.IpV4).IpV4;
             var ipV4Layer = (IpV4Layer) ipV4Datagram.ExtractLayer();
-            var tcpDatagram = ipV4Datagram.Tcp;
-            var tcpLayer = (TcpLayer) tcpDatagram.ExtractLayer();
 
-            var athernetEntry = new NatEntry(ProtocolType.Tcp, ipV4Layer.Source.ToString(), tcpLayer.SourcePort);
-
-            _natTable.TryGetValue(athernetEntry, out var ethernetEntry);
-            if (ethernetEntry == null)
+            if (ipV4Layer.Protocol == IpV4Protocol.InternetControlMessageProtocol)
             {
-                ethernetEntry = new NatEntry(ProtocolType.Tcp, _localAddress.ToString(),
-                    (ushort) _random.Next(8192, 65535));
-                _natTable.Add(athernetEntry, ethernetEntry);
-                _natTable.Add(ethernetEntry, athernetEntry);
-                _filterPorts.Add(ethernetEntry.Id);
-                _communicator.SetFilter(FilterString);
-                Console.WriteLine(FilterString);
+                var athernetEntry = new NatEntry(ProtocolType.Icmp, ipV4Layer.Source.ToString(), 0);
+
+                _natTable.TryGetValue(athernetEntry, out var ethernetEntry);
+                if (ethernetEntry == null)
+                {
+                    ethernetEntry = new NatEntry(ProtocolType.Icmp, _localAddress.ToString(), 0);
+                    _natTable.Add(athernetEntry, ethernetEntry);
+                    _natTable.Add(ethernetEntry, athernetEntry);
+                }
+
+                ipV4Layer.Source = new IpV4Address(ethernetEntry.Ip);
+                ipV4Layer.HeaderChecksum = null;
+                var modifiedPacket =
+                    PacketBuilder.Build(DateTime.Now, ipV4Layer, ipV4Datagram.Payload.ExtractLayer());
+                SendEthernetPacket(modifiedPacket);
             }
-            //
-            // if (ethernetEntry == null)
-            // {
-            //     ethernetEntry = new NatEntry(ProtocolType.Tcp, _localAddress.ToString(), (ushort) _random.Next(8192, 65535));
-            //     _natTable.Add(athernetEntry, ethernetEntry);
-            //     _filterPorts.Add(ethernetEntry.Id);
-            //     _communicator.SetFilter(FilterString);
-            // }
+            else
+            {
+                var tcpDatagram = ipV4Datagram.Tcp;
+                var tcpLayer = (TcpLayer) tcpDatagram.ExtractLayer();
 
-            
-            tcpLayer.SourcePort = ethernetEntry.Id;
-            ipV4Layer.Source = new IpV4Address(ethernetEntry.Ip);
-            tcpLayer.Checksum = ipV4Layer.HeaderChecksum = null;
+                var athernetEntry = new NatEntry(ProtocolType.Tcp, ipV4Layer.Source.ToString(), tcpLayer.SourcePort);
 
-            Console.WriteLine(
-                $"-> [NAT] {tcpLayer.ControlBits} Seq={tcpLayer.SequenceNumber} Win={tcpLayer.Window} Ack={tcpLayer.AcknowledgmentNumber} PayloadLen={tcpDatagram.PayloadLength}");
+                _natTable.TryGetValue(athernetEntry, out var ethernetEntry);
+                if (ethernetEntry == null)
+                {
+                    ethernetEntry = new NatEntry(ProtocolType.Tcp, _localAddress.ToString(),
+                        (ushort) _random.Next(8192, 65535));
+                    _natTable.Add(athernetEntry, ethernetEntry);
+                    _natTable.Add(ethernetEntry, athernetEntry);
+                    _filterPorts.Add(ethernetEntry.Id);
+                    _communicator.SetFilter(FilterString);
+                    Console.WriteLine(FilterString);
+                }
+                //
+                // if (ethernetEntry == null)
+                // {
+                //     ethernetEntry = new NatEntry(ProtocolType.Tcp, _localAddress.ToString(), (ushort) _random.Next(8192, 65535));
+                //     _natTable.Add(athernetEntry, ethernetEntry);
+                //     _filterPorts.Add(ethernetEntry.Id);
+                //     _communicator.SetFilter(FilterString);
+                // }
 
-            var modifiedPacket = PacketBuilder.Build(DateTime.Now, ipV4Layer, tcpLayer, tcpDatagram.Payload.ExtractLayer());
-            SendEthernetPacket(modifiedPacket);
+
+                tcpLayer.SourcePort = ethernetEntry.Id;
+                ipV4Layer.Source = new IpV4Address(ethernetEntry.Ip);
+                ipV4Layer.HeaderChecksum = null;
+                tcpLayer.Checksum = null;
+
+                Console.WriteLine(
+                    $"-> [NAT] {tcpLayer.ControlBits} Seq={tcpLayer.SequenceNumber} Win={tcpLayer.Window} Ack={tcpLayer.AcknowledgmentNumber} PayloadLen={tcpDatagram.PayloadLength}");
+
+                var modifiedPacket =
+                    PacketBuilder.Build(DateTime.Now, ipV4Layer, tcpLayer, tcpDatagram.Payload.ExtractLayer());
+                SendEthernetPacket(modifiedPacket);
+            }
         }
 
         private void SendEthernetPacket(Packet packet)
@@ -140,28 +166,51 @@ namespace Athernet.Nat
         {
             var ipV4Datagram = packet.Ethernet.IpV4;
             var ipV4Layer = (IpV4Layer) ipV4Datagram.ExtractLayer();
-            var tcpDatagram = ipV4Datagram.Tcp;
-            var tcpLayer = (TcpLayer) tcpDatagram.ExtractLayer();
 
-            var ethernetEntry =
-                new NatEntry(ProtocolType.Tcp, ipV4Layer.Destination.ToString(), tcpLayer.DestinationPort);
-
-            _natTable.TryGetValue(ethernetEntry, out var athernetEntry);
-
-            if (athernetEntry == null)
+            if (ipV4Layer.Protocol == IpV4Protocol.InternetControlMessageProtocol)
             {
-                throw new KeyNotFoundException("athernet entry not exist");
+                var ethernetEntry =
+                    new NatEntry(ProtocolType.Icmp, ipV4Layer.Destination.ToString(), 0);
+
+                _natTable.TryGetValue(ethernetEntry, out var athernetEntry);
+
+                if (athernetEntry == null)
+                {
+                    throw new KeyNotFoundException("athernet entry not exist");
+                }
+
+                ipV4Layer.Source = new IpV4Address(ethernetEntry.Ip);
+                ipV4Layer.HeaderChecksum = null;
+                var modifiedPacket =
+                    PacketBuilder.Build(DateTime.Now, ipV4Layer, ipV4Datagram.Payload.ExtractLayer());
+                SendEthernetPacket(modifiedPacket);
             }
+            else
+            {
+                var tcpDatagram = ipV4Datagram.Tcp;
+                var tcpLayer = (TcpLayer) tcpDatagram.ExtractLayer();
 
-            tcpLayer.DestinationPort = athernetEntry.Id;
-            ipV4Layer.CurrentDestination = new IpV4Address(athernetEntry.Ip);
-            tcpLayer.Checksum = ipV4Layer.HeaderChecksum = null;
-            
-            Console.WriteLine(
-                $"<- [NAT] {tcpLayer.ControlBits} Seq={tcpLayer.SequenceNumber} Win={tcpLayer.Window} Ack={tcpLayer.AcknowledgmentNumber} PayloadLen={tcpDatagram.PayloadLength}");
+                var ethernetEntry =
+                    new NatEntry(ProtocolType.Tcp, ipV4Layer.Destination.ToString(), tcpLayer.DestinationPort);
 
-            var modifiedPacket = PacketBuilder.Build(DateTime.Now, ipV4Layer, tcpLayer, tcpDatagram.Payload.ExtractLayer());
-            SendAthernetPacket(modifiedPacket);
+                _natTable.TryGetValue(ethernetEntry, out var athernetEntry);
+
+                if (athernetEntry == null)
+                {
+                    throw new KeyNotFoundException("athernet entry not exist");
+                }
+
+                tcpLayer.DestinationPort = athernetEntry.Id;
+                ipV4Layer.CurrentDestination = new IpV4Address(athernetEntry.Ip);
+                tcpLayer.Checksum = ipV4Layer.HeaderChecksum = null;
+
+                Console.WriteLine(
+                    $"<- [NAT] {tcpLayer.ControlBits} Seq={tcpLayer.SequenceNumber} Win={tcpLayer.Window} Ack={tcpLayer.AcknowledgmentNumber} PayloadLen={tcpDatagram.PayloadLength}");
+
+                var modifiedPacket =
+                    PacketBuilder.Build(DateTime.Now, ipV4Layer, tcpLayer, tcpDatagram.Payload.ExtractLayer());
+                SendAthernetPacket(modifiedPacket);
+            }
         }
 
         private void SendAthernetPacket(Packet packet)
--- ToyNet/Program.cs
+++ ToyNet/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Net;
+using System.Threading;
 using Athernet.AppLayer.AthernetFTPClient;
 using Athernet.MacLayer;
 using Athernet.Nat;
+using Athernet.Sockets;
+using PcapDotNet.Packets.IpV4;
 
 namespace ToyNet
 {
@@ -12,15 +15,19 @@ namespace ToyNet
         {
             Athernet.Utils.Audio.ListDevices();
             
-            var node2 = new Mac(0, 4, 3, 10240);
-            var nat = new Nat(1, node2);
-            nat.Listen();
+            // var node2 = new Mac(0, 4, 3, 10240);
+            // var nat = new Nat(1, node2);
+            // nat.Listen();
             
             Console.Write("Connecting Host Address:");
-            String Address = Console.ReadLine();
-            var CLI = new UserInterface(Address);
+            // String Address = Console.ReadLine();
+            // var CLI = new UserInterface("ftp.sjtu.edu.cn");
             //var CLI = new UserInterface("140.110.96.68");
-            CLI.Shell();
+            // CLI.Shell();
+            var node1 = new Mac(1, 2, 0, 10240);
+            var icmpSocket = new AthernetIcmpSocket(node1);
+            icmpSocket.SendIcmpPacket(new IpV4Address("10.20.200.145"));
+            Thread.Sleep(2333333);
         }
     }
 }
